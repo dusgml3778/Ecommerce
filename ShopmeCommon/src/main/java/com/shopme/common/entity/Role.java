77b4fa4e1c74dff05d63006ea86090c5c4a049cd
kimmingye <dusgml3779@gmail.com>
2022-11-24 21:34:09 +0900

    jpa test code

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
@@ -7,33 +7,36 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
-@Table(name = "roles")
+@Entity // 엔티티 클래스 임을 나타냄
+@Table(name = "roles") // 테이블 명
 public class Role {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
-	@Column(length = 40 ,nullable = false ,unique = true)
+
+	@Column(length = 40, nullable = false, unique = true)
 	private String name;
-	
-	@Column(length = 150 ,nullable = false)
+
+	@Column(length = 150, nullable = false)
 	private String description;
-	
+
 	public Role() {
-		
+
 	}
 	
-	
+	public Role(Integer id) {
+		this.id = id;
+	}
+
+
 	public Role(String name) {
-		
+
 		this.name = name;
 	}
-	
 
 	public Role(String name, String description) {
-	
+
 		this.name = name;
 		this.description = description;
 	}
@@ -60,6 +63,36 @@ public class Role {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [name=" + name + "]";
 	}
 
 }
