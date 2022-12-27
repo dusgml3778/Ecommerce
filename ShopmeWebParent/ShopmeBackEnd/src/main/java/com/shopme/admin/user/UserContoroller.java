36814bfd7184502ba9a5dc7bc2218cc341759b00
kimmingye <dusgml3779@gmail.com>
2022-12-27 10:42:13 +0900

    Add FileUproading

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserContoroller.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserContoroller.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.common.entity.Role;
@@ -40,12 +42,14 @@ public class UserContoroller {
 	}
 
 	@PostMapping("/users/save")
-	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+	public String saveUser(User user, RedirectAttributes redirectAttributes,
+		@RequestParam("image") MultipartFile multipartFile) {
 
 		System.out.println(user.toString());
-		service.save(user);
+		System.out.println(multipartFile.getOriginalFilename());
+		//service.save(user);
 
-		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+		//redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
 		return "redirect:/users";
 	}
 
